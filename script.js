25cc5544704c376d760734ecaf87a98ae6e885c8
lewis-0870 <lewis221087@gmail.com>
2025-09-07 19:59:42 +0100

    Working
    Branch save

--- script.js
+++
@@ -1,6 +0,0 @@
-const myImage = document.getElementById('myImage');
-myImage.addEventListener('click', () => {
-  myImage.classList.add('pop-up');
-  document.getElementById("hidecard").style.display = "block";
-  hidecard.classList.add('showcard');
-});
