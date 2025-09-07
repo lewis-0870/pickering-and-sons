79d2b45304c45e372e36ee2d443fee49dd521fd0
lewis-0870 <lewis221087@gmail.com>
2025-09-07 18:12:09 +0100

    Update

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ const myImage = document.getElementById('myImage');
 myImage.addEventListener('click', () => {
   myImage.classList.add('pop-up');
   document.getElementById("hidecard").style.display = "block";
+  hidecard.classList.add('showcard');
 });
